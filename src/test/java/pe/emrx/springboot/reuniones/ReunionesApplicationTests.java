36035e068780f18a7941497c507909da8def5137
Elvis Maldonado Rodriguez <u201112959@upc.edu.pe>
2023-12-30 22:21:08 -0500

    Fundamentos de Spring Boot

---
+++ src/test/java/pe/emrx/springboot/reuniones/ReunionesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.emrx.springboot.reuniones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReunionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
