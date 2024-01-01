7bdf8dc3b26d5077196e85e68086bb3a1217e3a4
Elvis Maldonado Rodriguez <u201112959@upc.edu.pe>
2023-12-31 19:25:55 -0500

    Fin del proyecto - se agrego docker-compose para rabbitmq, proyecto complementario busca para la demostración de mensajeria de cola

---
+++ busca/src/test/java/pe/emrx/springboot/busca/BuscaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.emrx.springboot.busca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
